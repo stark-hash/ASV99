7f02acd932ba6a693ee7f2c20be8c5cd0c8b8c3c
stark-hash <91049862+stark-hash@users.noreply.github.com>
2024-07-17 04:51:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -30,8 +30,8 @@ BOT_START_TIME = time()
 
 # Bot images & videos
 PICS = (environ.get('PICS', 'https://telegra.ph/file/f00aa611bc7d78653be2d.jpg https://telegra.ph/file/1017ebafed9cf26918f91.jpg https://telegra.ph/file/8634cdb325b0adf448232.jpg https://telegra.ph/file/768d1068021849536c37a.jpg https://telegra.ph/file/aff503e002443f6bcc621.jpg')).split()
-REQ_PICS = (environ.get('REQ_PICS', 'https://graph.org/file/f4c687fe798c1cf5e98cb.jpg')).split()
-NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/file/0593a3103ba1b9a5855bf.jpg")
+REQ_PICS = (environ.get('REQ_PICS', 'https://telegra.ph/file/f4c687fe798c1cf5e98cb.jpg')).split()
+NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/file/f4c687fe798c1cf5e98cb.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/file/ecaf5f143fd622772abdc.mp4")
 SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/2a888a370f479f4338f7c.jpg")
 
