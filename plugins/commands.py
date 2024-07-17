c03dcb182a1a40d0dc6c3dd3cbda511aec7641de
stark-hash <91049862+stark-hash@users.noreply.github.com>
2024-07-17 12:15:31 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -20,7 +20,7 @@ BATCH_FILES = {}
 
 @Client.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
-    if AUTH_CHANNEL:
+    if AUTH_CHANNELS:
         try:
             btn = await is_subscribed(client, message, AUTH_CHANNELS)
             if btn:
